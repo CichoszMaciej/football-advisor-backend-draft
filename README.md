7b0c39a3e0d818eeba8aa9c50c6a11f4a27d790b
Maciej Cichosz <cichosz.maciej@gmail.com>
2020-08-08 18:27:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# football-advisor-backend
+Internship 07.2020 football advisor backend service
---
+++ src/test/java/com/softylabs/FootballAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softylabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
